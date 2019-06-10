2b6fdf7465935e36817f4d9f89826fb33ef91114
Alex Kharchenko <doom9936.1999@gmail.com>
2019-06-10 21:57:56 +0300

    add fifth test project

---
+++ fifth_lesson/main.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './public/storage/img/vmSocks-green-onWhite.jpg',
+        inventory: 100,
+        details: ["90% cotton", "10% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./public/storage/img/vmSocks-green-onWhite.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./public/storage/img/vmSocks-blue-onWhite.jpg"
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL'],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            ++this.cart
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            if(this.cart != 0) {
+                --this.cart
+            }
+        }
+    }
+});
