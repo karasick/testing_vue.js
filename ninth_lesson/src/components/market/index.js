45732fccc07966eea868798c3e2a643eab49e479
Alex Kharchenko <doom9936.1999@gmail.com>
2019-06-15 11:56:42 +0300

    add ninth test project

---
+++ ninth_lesson/src/components/market/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+
+Vue.component('cart', require('./Cart').default);
+Vue.component('product', require('./Product').default);
+Vue.component('product-details', require('./ProductDetails').default);
